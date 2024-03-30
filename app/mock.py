d5619c8a699bc9b28d3015a29e467417ff5b781e
Zhukov Georgiy <zhugeo@mail.ru>
2024-03-30 22:57:28 +0300

    Небольшие изменения перед заливкой на github

---
+++ app/mock.py
@@ -0,0 +1,127 @@
+QUESTIONS=[
+    {
+        "id":1,
+        "title":"Who is in this picture?",
+        "text":"I though very long",
+        "image":"img/president.jpg",
+        "rating":5,
+        "answers":[
+            {
+        "avatar":"img/president.jpg",
+        "text":"Maybe, its president",
+        "rating":2,
+        "user":"mr_beast"
+            }
+        ]
+    },
+    {
+        "id":2,
+        "title": "What is the capital of France?",
+        "text": "Test your geography knowledge",
+        "image": "img/president.jpg",
+        "rating": 4,
+        "answers": [
+            {
+                "avatar": "img/president.jpg",
+                "text": "The capital of France is Paris",
+                "rating": 5,
+                "user": "geo_expert"
+            }
+        ]
+    },
+    {
+        "id":3,
+        "title": "Who wrote the famous play 'Romeo and Juliet'?",
+        "text": "A classic literary question",
+        "image": "img/president.jpg",
+        "rating": 4,
+        "answers": [
+            {
+                "avatar": "img/president.jpg",
+                "text": "William Shakespeare wrote 'Romeo and Juliet'",
+                "rating": 5,
+                "user": "bardfan"
+            },
+            {
+                "avatar": "img/president.jpg",
+                "text": "It was penned by an anonymous playwright",
+                "rating": 4,
+                "user": "literary_buff"
+            }
+        ]
+    },
+    {
+        "id":4,
+        "title":"Who is in this picture?",
+        "text":"I though very long",
+        "image":"img/president.jpg",
+        "rating":5,
+        "answers":[
+            {
+        "avatar":"img/president.jpg",
+        "text":"Maybe, its president",
+        "rating":2,
+        "user":"mr_beast"
+            }
+        ]
+        },
+        
+    {
+        "id":5,
+        "title": "What is the capital of France?",
+        "text": "Test your geography knowledge",
+        "image": "img/president.jpg",
+        "rating": 4,
+        "answers": [
+            {
+                "avatar": "img/president.jpg",
+                "text": "The capital of France is Paris",
+                "rating": 5,
+                "user": "geo_expert"
+            }
+        ]
+    },
+    {
+        "id":6,
+        "title": "Who wrote the famous play 'Romeo and Juliet'?",
+        "text": "A classic literary question",
+        "image": "img/president.jpg",
+        "rating": 4,
+        "answers": [
+            {
+                "avatar": "img/president.jpg",
+                "text": "William Shakespeare wrote 'Romeo and Juliet'",
+                "rating": 5,
+                "user": "bardfan"
+            },
+            {
+                "avatar": "img/president.jpg",
+                "text": "It was penned by an anonymous playwright",
+                "rating": 4,
+                "user": "literary_buff"
+            }
+        ]
+    },
+    {
+        "id":7,
+        "title": "What is the largest mammal on Earth?",
+        "text": "Test your zoological knowledge",
+        "image": "img/whale.jpg",
+        "rating": 4,
+        "answers": [
+            {
+                "avatar": "img/blue_whale.jpg",
+                "text": "The largest mammal is the blue whale",
+                "rating": 5,
+                "user": "marine_biologist"
+            },
+            {
+                "avatar": "img/elephant.jpg",
+                "text": "It's the African elephant",
+                "rating": 2,
+                "user": "animal_lover"
+            }
+        ]
+    },
+
+]
